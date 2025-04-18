0e911044f96c184a7b7240ee6413a122448f77be
mohan-bee <mohn08052006@gmail.com>
2025-04-18 16:11:13 +0530

    Family Dashboard v1 Completed

--- client/src/stores/useFamilyStore.js
+++ client/src/stores/useFamilyStore.js
@@ -4,11 +4,29 @@ import toast from 'react-hot-toast'
 
 const useFamilyStore = create((set) => ({
     family: null,
+    getFamily: async() => {
+        try {
+            let res = await axiosInstance.get('/family')
+            console.log("Family Res", res)
+            set({family:res.data.family})
+        } catch (error) {
+            console.log(error.message)
+        }
+    },
     createFamily: async(familyName) => {
         try {
             const res = await axiosInstance.post('/family/create', {name: familyName})
             toast.success(`${familyName} Created Successfully !`)
             console.log(res.data)
+            set({family: res.data.newFamily})
+        } catch (error) {
+            console.log(error.message)
+        }
+    },
+    joinFamilyMember: async(uniqueCode) => {
+        try {
+            const res = await axiosInstance.post('/family/join', {uniqueCode})
+            console.log(res)
         } catch (error) {
             console.log(error.message)
         }
--- server/routes/family.route.js
+++ server/routes/family.route.js
@@ -1,10 +1,11 @@
-const {createFamily,getFamily,addFamilyMember,removeFamilyMember,deleteFamily} = require('../controllers/family.controller')
+const {createFamily,getFamily,addFamilyMember,removeFamilyMember,deleteFamily, joinFamilyMember} = require('../controllers/family.controller')
 const router = require('express').Router()
 
 router.post('/create', createFamily)
 router.get('/', getFamily)
 router.post('/add-member', addFamilyMember)
 router.post('/remove-member', removeFamilyMember)
+router.post('/join', joinFamilyMember)
 router.delete('/:id', deleteFamily)
 
 module.exports  = router
