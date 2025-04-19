ca9b44e1c0978ef4d8538857f9f56897a1600bb1
mohan-bee <mohn08052006@gmail.com>
2025-04-19 11:16:18 +0530

    Cookie Updates

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -38,6 +38,7 @@ const login = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
+            sameSite: 'strict',
             maxAge: 7 * 24 * 60 * 60 * 1000,
         });
 
