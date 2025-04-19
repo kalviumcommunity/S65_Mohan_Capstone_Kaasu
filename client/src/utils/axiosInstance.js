ee99b2ae453c23d9dbd88ff5b4eab4312c228e3b
mohan-bee <mohn08052006@gmail.com>
2025-04-19 10:14:18 +0530

    Test v1

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // const axiosInstance = axios("http://localhost:3000/api", {withCredentials: true})
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:3000/api",
+    baseURL: `${import.meta.env.VITE_API_BACKED_URL}/api`,
     withCredentials: true
 })
 
--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const cors = require('cors')
 app.use(cookieParser())
 app.use(express.json())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "*",
     credentials: true
 }))
 app.use('/api/auth', require('./routes/user.route'))
