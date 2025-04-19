fc3dc729ae5aac2bb1b8b16dca2af3702574d0bd
mohan-bee <mohn08052006@gmail.com>
2025-04-19 11:24:33 +0530

    New Changes

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -45,6 +45,7 @@ const login = async (req, res) => {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
             sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+            domain: '.vercel.app',
             maxAge: 7 * 24 * 60 * 60 * 1000,
         }) 
 
