2cbc418c81c5c31862f590e8c27b49fc5f5f2678
Mohan <167054270+mohan-bee@users.noreply.github.com>
2025-04-19 11:39:17 +0530

    Update user.controller.js

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -36,12 +36,11 @@ const login = async (req, res) => {
         );
 
         res.cookie("token", token, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-            domain: '.vercel.app',
-            maxAge: 7 * 24 * 60 * 60 * 1000,
-        });
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true, 
+    sameSite: "strict", 
+    secure: process.env.NODE_ENV !== "development",
+  });
 
         return res.status(200).json({ msg: "User Logged In Successfully", user });
     } catch (error) {
