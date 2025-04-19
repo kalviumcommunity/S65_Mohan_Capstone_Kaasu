def7368f154845d9c756d28498722f34444f31bf
mohan-bee <mohn08052006@gmail.com>
2025-04-19 12:44:09 +0530

    Kaasu v1 Completed

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,11 @@
+// client/vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: '.', // This should point to client root (default is fine)
+  build: {
+    outDir: 'dist'
+  }
 })
