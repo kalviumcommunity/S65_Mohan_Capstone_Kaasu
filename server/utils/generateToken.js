14e3cd0746aa461076744c38e923af5aa850031f
mohan-bee <mohn08052006@gmail.com>
2025-08-01 22:07:01 +0530

    fix: login redirection

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -4,8 +4,8 @@ exports.generateToken = (user, res) => {
     const token = jwt.sign({userId: user._id,familyId: user.familyId, username: user.username}, process.env.JWT_SECRET, {expiresIn:'7d'})
         res.cookie("token", token, {
             maxAge: 7 * 24 * 60 * 60 * 1000,
-            httpOnly:  process.env.NODE_ENV != "developement",
+            httpOnly:  process.env.NODE_ENV != "development",
             sameSite: 'none',
-            secure: process.env.NODE_ENV != "developement",
+            secure: process.env.NODE_ENV != "development",
         })
 }
